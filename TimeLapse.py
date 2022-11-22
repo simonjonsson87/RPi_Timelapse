14614a2eadd56a9d7fdc6f3cc5cd8c87ecd82e62
Simon Jonsson <simonjonsson87@gmail.com>
2022-11-22 22:13:35 +0000

    Maybe done version

--- TimeLapse.py
+++ TimeLapse.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date, time
 import os
-import time
+import time, shutil
 #import psutil
 #disk = psutil.disk_usage('/')
 #disk_free = disk.free / 2**30 # GiB
@@ -8,38 +8,34 @@ import time
 #PiLapse, originally written by The HatMan
 #Note, all of the print lines can be commented out and are not necessary
 
-active = False
+start = time(hour=4, minute=0, second=0)
+end = time(hour=16, minute=0, second=0)
+INTERVAL = 3000
+FOLDER = '/home/pi/time-lapse/images/' + datetime.now().strftime("%Y-%m-%d_%H%M%S")
+FOLDER_current = '/home/pi/time-lapse/images/'
 
-#counts repeats of the while loop, can be commented out.
-counter = 0
 
-print('To escape loop, use Ctrl+C')
-print()
-
-INTERVAL = 708.0
-FOLDER = '/home/pi/Pictures/'
 
 timerstart = time.time()
-
-com = 'raspistill -o current.jpg -q 100'
-com = 'raspistill -o /home/pi/Pictures/current.jpg -q 100'
 os.system('raspistill -o /home/pi/Pictures/current.jpg -q 100')# -w 1920 -h 1080')
 
-print(time.time() - timerstart)
-
+active = True
 while active is True:
         
-    # Check if the INTERVAL has passed yet.
-	if (time.time() - timerstart > INTERVAL):
-		#print(time.time() - timerstart)
-		os.system('raspistill -o /home/pi/Pictures/current.jpg -q 100')
-		#os.rename(FOLDER + 'current.jpg', FOLDER + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.jpg')
-		#print('renamed the file current.jpg')
-		timerstart = time.time()
+	
+	current = datetime.now().time()
+	if current > start and current < end:
 
-   #if current.jpg exists, rename it
-	if (os.path.isfile('/home/pi/Pictures/current.jpg')):
-		os.rename(FOLDER + 'current.jpg', FOLDER + datetime.now().strftime("%Y-%m-%d %H%M%S") + '.jpg')
+		# Check if the INTERVAL has passed yet.
+		if (time.time() - timerstart > INTERVAL):
+			os.system('raspistill -o ' + FOLDER + 'current.jpg -q 100')
+			timerstart = time.time()
+
+		if (os.path.isfile(FOLDER + 'current.jpg')):
+			shutil.copyfile(FOLDER + 'current.jpg', FOLDER_current + 'current.jpg')
+			os.rename(FOLDER + 'current.jpg', FOLDER + datetime.now().strftime("%Y-%m-%d %H%M%S") + '.jpg')
+
+	time.sleep(1000)		
 	
 	
 	

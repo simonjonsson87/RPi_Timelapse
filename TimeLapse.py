0f6da581a4cb0541a43a5b4f6efa0f3b9f3013f1
Simon <simonjonsson87@gmail.com>
2022-11-22 23:08:25 +0000

    Functional v1

--- TimeLapse.py
+++ TimeLapse.py
@@ -1,6 +1,6 @@
-from datetime import datetime, date, time
-import os
-import time, shutil
+
+import os, datetime, time
+import shutil
 #import psutil
 #disk = psutil.disk_usage('/')
 #disk_free = disk.free / 2**30 # GiB
@@ -8,34 +8,43 @@ import time, shutil
 #PiLapse, originally written by The HatMan
 #Note, all of the print lines can be commented out and are not necessary
 
-start = time(hour=4, minute=0, second=0)
-end = time(hour=16, minute=0, second=0)
-INTERVAL = 3000
-FOLDER = '/home/pi/time-lapse/images/' + datetime.now().strftime("%Y-%m-%d_%H%M%S")
+start = datetime.time(hour=4, minute=0, second=0)
+end = datetime.time(hour=23, minute=0, second=0)
+INTERVAL = 10*60
+FOLDER = '/home/pi/time-lapse/images/' + datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S") + '/'
 FOLDER_current = '/home/pi/time-lapse/images/'
 
+isExist = os.path.exists(FOLDER)
+if not isExist:
+   os.makedirs(FOLDER)
 
 
 timerstart = time.time()
-os.system('raspistill -o /home/pi/Pictures/current.jpg -q 100')# -w 1920 -h 1080')
+os.system('raspistill -o ' + FOLDER + 'current.jpg -q 100')
+
+
 
 active = True
 while active is True:
         
 	
-	current = datetime.now().time()
+	current = datetime.datetime.now().time()
 	if current > start and current < end:
+		print('in loop')
+		print(time.time() - timerstart)
 
 		# Check if the INTERVAL has passed yet.
 		if (time.time() - timerstart > INTERVAL):
+			print('interval')
 			os.system('raspistill -o ' + FOLDER + 'current.jpg -q 100')
 			timerstart = time.time()
 
 		if (os.path.isfile(FOLDER + 'current.jpg')):
+			print('isfile')
 			shutil.copyfile(FOLDER + 'current.jpg', FOLDER_current + 'current.jpg')
-			os.rename(FOLDER + 'current.jpg', FOLDER + datetime.now().strftime("%Y-%m-%d %H%M%S") + '.jpg')
+			os.rename(FOLDER + 'current.jpg', FOLDER + datetime.datetime.now().strftime("%Y-%m-%d %H%M%S") + '.jpg')
 
-	time.sleep(1000)		
+	time.sleep(1)		
 	
 	
 	
